ef4fc4485e361f940860f064a21e7b71f20f3add
lishengshi <lishengshi123>
2019-06-06 11:14:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+hadoop-mapreduce的练习项目；
+源码阅读项目- 画类图
---
+++ src/main/java/mapreduce/hello.java
@@ -0,0 +1,11 @@
+package mapreduce;
+
+/**
+ * @author lss
+ * @create 2019-06-06 11:10
+ **/
+public class hello {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
